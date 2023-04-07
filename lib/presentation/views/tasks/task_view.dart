6d12e927cf191105acce7a6ace3bb97feeb346c1
fatihdursun.uzer <fatihdursun.uzer@albil.com.tr>
2023-04-07 16:12:14 +0300

    timer added to task view model

--- lib/presentation/views/tasks/task_view.dart
+++ lib/presentation/views/tasks/task_view.dart
@@ -1,7 +1,10 @@
+import 'dart:async';
+
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:provider/provider.dart';
 import 'package:task_time_tracker/presentation/generated/locale_keys.g.dart';
 import 'package:task_time_tracker/presentation/views/tasks/task_view_model.dart';
@@ -17,37 +20,71 @@ class TaskView extends StatefulWidget {
 
 class _TaskViewState extends State<TaskView> {
   @override
+  void initState() {
+    super.initState();
+  }
+
+  String format(Duration d) => d.toString().split('.').first.padLeft(8, "0");
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: FunctionalAppBar(),
-        body: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(32.0),
-            child: Column(
-              children: [
-                Text(
-                  context
-                      .watch<TaskViewModel>()
-                      .currentTask
-                      .tags!
-                      .first
-                      .toString(),
-                ),
-                Text(
-                  context
-                      .watch<TaskViewModel>()
-                      .currentTask.description
-                      .toString(),
-                ),
-                CustomButton(
-                    text: LocaleKeys.finish.tr(),
-                    onPressed: () {
-                      //context.read<TaskViewModel>().finishTask();
-                    }),
-                TextButton(onPressed: () {}, child: Text(LocaleKeys.quit.tr()))
-              ],
+        appBar: AppBar(
+            title: Text(context.read<TaskViewModel>().currentTask.title)),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text(
+              context.read<TaskViewModel>().currentTask.description,
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
             ),
-          ),
+            stopWatch(context),
+            TimeIconsRow(context),
+          ],
         ));
+  }
+
+  Text stopWatch(BuildContext context) {
+    return Text(format(context.read<TaskViewModel>().duration),
+        style: TextStyle(fontSize: 30));
+  }
+
+  Row TimeIconsRow(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        !context.watch<TaskViewModel>().isStarted
+            ? IconButton(
+                onPressed: () {
+                  context.read<TaskViewModel>().startTimer();
+                },
+                icon: const FaIcon(FontAwesomeIcons.solidPlayCircle, size: 50))
+            : IconButton(
+                onPressed: () {
+                  context.read<TaskViewModel>().stopTimer();
+                },
+                icon:
+                    const FaIcon(FontAwesomeIcons.solidPauseCircle, size: 50)),
+        IconButton(
+            onPressed: () {},
+            icon: FaIcon(FontAwesomeIcons.solidStopCircle, size: 50)),
+      ],
+    );
+  }
+
+  appBarActions() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          context.read<TaskViewModel>().currentTask.title,
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+        Text(
+          context.read<TaskViewModel>().currentTask.tags![0].toString(),
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+      ],
+    );
   }
 }
--- lib/presentation/views/tasks/task_view_model.dart
+++ lib/presentation/views/tasks/task_view_model.dart
@@ -1,7 +1,35 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:task_time_tracker/core/domain/entities/Tasks/task.dart';
 
 class TaskViewModel extends ChangeNotifier {
+  bool _isStarted = false;
+  bool get isStarted => _isStarted;
+  Duration _duration = Duration();
+  Timer? _timer;
+
+  Duration get duration => _duration;
+
+  void startTimer() {
+    _isStarted = true;
+    _timer = Timer.periodic(Duration(seconds: 1), (timer) {
+      _duration = _duration + Duration(seconds: 1);
+      notifyListeners();
+    });
+  }
+
+  void stopTimer() {
+    _timer?.cancel();
+    _isStarted = false;
+    notifyListeners();
+  }
+
+  void set isStarted(bool isStarted) {
+    _isStarted = isStarted;
+    notifyListeners();
+  }
+
   Task _currentTask = Task.init();
 
   Task get currentTask => _currentTask;
